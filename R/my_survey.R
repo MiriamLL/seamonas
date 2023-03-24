b8fee38607fcdb454d78b0ab529520f569015a13
Miriam Lerma <miriamjlerma@gmail.com>
2023-03-24 13:36:54 +0100

    function to create grid

---
+++ R/my_survey.R
@@ -0,0 +1,11 @@
+#' Random generated data
+#' 2000 observations from nine variables.
+#' @docType data
+#' @usage data(my_survey)
+#' @format Data frame with random generated observations and nine variables
+#' @keywords datasets
+#' @references Lerma et al. 2023
+#' @examples
+#' data(my_survey)
+"my_survey"
+
