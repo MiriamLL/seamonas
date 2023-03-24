7aaf57d2b58434cc1efe13f7d055a33c7c2beae2
Miriam Lerma <miriamjlerma@gmail.com>
2023-03-24 17:46:31 +0100

    Update data names

---
+++ R/survey_3035.R
@@ -0,0 +1,10 @@
+#' Random generated data
+#' 2000 observations from nine variables.
+#' @docType data
+#' @usage data(survey_3035)
+#' @format Data frame with random generated observations and nine variables
+#' @keywords datasets
+#' @references Lerma et al. 2023
+#' @examples
+#' data(survey_3035)
+"survey_3035"
