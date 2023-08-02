48e42010eccdfa3654c03ed9710f2d1a199569d4
Miriam Lerma <miriamjlerma@gmail.com>
2023-08-02 11:22:43 +0200

    Update plots causing errors

---
+++ R/density_wmap.R
@@ -0,0 +1,10 @@
+#' A plot
+#' Create density plot in ggplot2.
+#' @docType data
+#' @usage data(density_wmap)
+#' @format List of 9 (ggplot format)
+#' @keywords datasets
+#' @references Lerma et al. 2023
+#' @examples
+#' data(density_wmap)
+"density_wmap"
