78fe4e01c2d353277813f559f64dfaabd6f025fd
Miriam Lerma <miriamjlerma@gmail.com>
2025-06-18 13:29:47 +0200

    Add Basis and Trip Daten

---
+++ R/BasisDaten_test.R
@@ -0,0 +1,9 @@
+#' BasisDaten_test contains BasisDaten Information
+#' @docType data
+#' @usage data(BasisDaten_test)
+#' @format Data frame with 74440 obs and 22 columns
+#' @keywords datasets
+#' @references DDA
+#' @examples
+#' data(BasisDaten_test)
+"BasisDaten_test"
---
+++ R/TripDaten_test.R
@@ -0,0 +1,9 @@
+#' TripDaten_test contains TripDaten Information
+#' @docType data
+#' @usage data(TripDaten_test)
+#' @format Data frame with 1 obs and 36 columns
+#' @keywords datasets
+#' @references DDA
+#' @examples
+#' data(TripDaten_test)
+"TripDaten_test"
