157ac7e3bd3a8b6f703eb5595e6799f9baf98ccb
Miriam Lerma <miriamjlerma@gmail.com>
2025-06-18 14:12:11 +0200

    Add code descriptions

---
+++ R/Code_Descriptions.R
@@ -0,0 +1,9 @@
+#' Descriptions include information for filling up the columns
+#' @docType data
+#' @usage data(Code_Descriptions)
+#' @format Data frame with 70 obs and 18 variables
+#' @keywords datasets
+#' @references DDA
+#' @examples
+#' data(Code_Descriptions)
+"Code_Descriptions"
