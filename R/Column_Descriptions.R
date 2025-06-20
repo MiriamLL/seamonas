f086d01ec82ab392e2318ff9fc87a1af879aa910
Miriam Lerma <miriamjlerma@gmail.com>
2025-06-20 17:25:29 +0200

    add column descriptions

---
+++ R/Column_Descriptions.R
@@ -0,0 +1,9 @@
+#' Includes column names and explanation for filling them.
+#' @docType data
+#' @usage data(Column_Descriptions)
+#' @format Data frame with column names and explanations.
+#' @keywords datasets
+#' @references DDA
+#' @examples
+#' data(Column_Descriptions)
+"Column_Descriptions"
